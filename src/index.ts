61b445b523ac671794915cb12fd171c5ea76faac
caw-ishikajain <ishika@reeco.io>
2022-08-25 13:13:42 +0530

    Add a helper

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,7 @@ export const sum = (a: number, b: number) => {
   if ('development' === process.env.NODE_ENV) {
     console.log('boop');
   }
+  let result = new Greeter('Ishika');
+  console.log(result.greet());
   return a + b;
 };
